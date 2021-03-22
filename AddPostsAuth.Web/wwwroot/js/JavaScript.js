10641ac591a4c56723f523fdc5a75e6d611f432d
Elka Samber <ersamber@gmail.com>
2021-03-21 21:10:02 -0400

    added js

---
+++ AddPostsAuth.Web/wwwroot/js/JavaScript.js
@@ -0,0 +1,14 @@
+﻿$(() => {
+    $(".form-control").on('keyup', function () {
+        ensureFormValidity();
+    })
+    function ensureFormValidity() {
+        const isValid = isFormValid();
+        $("#submit").prop('disabled', !isValid);
+    }
+    function isFormValid() {
+        const email = $("#email").val();
+        const password = $("#password").val();
+        return email && password;
+    }
+})
